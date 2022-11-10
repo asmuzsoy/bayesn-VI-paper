07e6d2bc63023e10739195af98e380962333fa17
mattgrayling <mjpg1g18@soton.ac.uk>
2022-11-10 17:04:50 +0000

    Initial commit

---
+++ model.py
@@ -0,0 +1,242 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pyro
+import pyro.distributions as dist
+import numpyro
+from numpyro.infer import MCMC, NUTS, Predictive
+import numpyro.distributions as dist
+from jax import random
+import h5py
+import lcdata
+import sncosmo
+from settings import parse_settings
+import torch
+import spline_utils
+
+
+class Model(object):
+    def __init__(self, bands, ignore_unknown_settings=False, settings={}, device='cpu'):
+        self.device = device
+        self.settings = parse_settings(bands, settings,
+                                       ignore_unknown_settings=ignore_unknown_settings)
+        self.M0 = -19.5
+
+        self.l_knots = np.genfromtxt('model_files/T21_model/l_knots.txt')
+        self.tau_knots = np.genfromtxt('model_files/T21_model/tau_knots.txt')
+        self.W0 = torch.from_numpy(np.genfromtxt('model_files/T21_model/W0.txt'))
+        self.W1 = torch.from_numpy(np.genfromtxt('model_files/T21_model/W1.txt'))
+        self.band_dict = {band: i for i, band in enumerate(bands)}
+
+        self._setup_band_weights()
+
+        KD_l = spline_utils.invKD_irr(self.l_knots)
+        self.J_l_T = torch.from_numpy(spline_utils.spline_coeffs_irr(self.model_wave, self.l_knots, KD_l)).float()
+        self.KD_t = spline_utils.invKD_irr(self.tau_knots)
+        self.load_hsiao_template()
+
+    def load_hsiao_template(self):
+        with h5py.File(os.path.join('data', 'hsiao.h5'), 'r') as file:
+            data = file['default']
+
+            hsiao_phase = data['phase'][()].astype('float64')
+            hsiao_wave = data['wave'][()].astype('float64')
+            hsiao_flux = data['flux'][()].astype('float64') * 10 ** (-0.4 * self.M0)
+
+        KD_l_hsiao = spline_utils.invKD_irr(hsiao_wave)
+        self.KD_t_hsiao = spline_utils.invKD_irr(hsiao_phase)
+        self.J_l_T_hsiao = torch.from_numpy(spline_utils.spline_coeffs_irr(self.model_wave,
+                                                                           hsiao_wave, KD_l_hsiao)).float()
+
+        self.hsiao_t = hsiao_phase
+        self.hsiao_l = hsiao_wave
+        self.hsiao_flux = torch.from_numpy(hsiao_flux).T.float()
+
+
+    def _setup_band_weights(self):
+        """Setup the interpolation for the band weights used for photometry"""
+        # Build the model in log wavelength
+        model_log_wave = np.linspace(np.log10(self.settings['min_wave']),
+                                     np.log10(self.settings['max_wave']),
+                                     self.settings['spectrum_bins'])
+
+        model_spacing = model_log_wave[1] - model_log_wave[0]
+
+        band_spacing = model_spacing / self.settings['band_oversampling']
+        band_max_log_wave = (
+                np.log10(self.settings['max_wave'] * (1 + self.settings['max_redshift']))
+                + band_spacing
+        )
+
+        # Oversampling must be odd.
+        assert self.settings['band_oversampling'] % 2 == 1
+        pad = (self.settings['band_oversampling'] - 1) // 2
+        band_log_wave = np.arange(np.log10(self.settings['min_wave']),
+                                  band_max_log_wave, band_spacing)
+        band_wave = 10 ** (band_log_wave)
+        band_pad_log_wave = np.arange(
+            np.log10(self.settings['min_wave']) - band_spacing * pad,
+            band_max_log_wave + band_spacing * pad,
+            band_spacing
+        )
+        band_pad_dwave = (
+                10 ** (band_pad_log_wave + band_spacing / 2.)
+                - 10 ** (band_pad_log_wave - band_spacing / 2.)
+        )
+
+        ref = sncosmo.get_magsystem(self.settings['magsys'])
+
+        band_weights = []
+
+        for band_name in self.settings['bands']:
+            band = sncosmo.get_bandpass(band_name)
+
+            band_transmission = band(10 ** (band_pad_log_wave))
+
+            # Convolve the bands to match the sampling of the spectrum.
+            band_conv_transmission = np.convolve(
+                band_transmission * band_pad_dwave,
+                np.ones(self.settings['band_oversampling']),
+                mode='valid'
+            )
+
+            band_weight = (
+                    band_wave
+                    * band_conv_transmission
+                    / sncosmo.constants.HC_ERG_AA
+                    / ref.zpbandflux(band)
+                    * 10 ** (0.4 * -20.)
+            )
+
+            band_weights.append(band_weight)
+
+        # Get the locations that should be sampled at redshift 0. We can scale these to
+        # get the locations at any redshift.
+        band_interpolate_locations = torch.arange(
+            0,
+            self.settings['spectrum_bins'] * self.settings['band_oversampling'],
+            self.settings['band_oversampling']
+        )
+
+        # Save the variables that we need to do interpolation.
+        self.band_interpolate_locations = band_interpolate_locations.to(self.device)
+        self.band_interpolate_spacing = band_spacing
+        self.band_interpolate_weights = torch.FloatTensor(band_weights).to(self.device)
+        self.model_wave = 10 ** (model_log_wave)
+
+    def _calculate_band_weights(self, redshifts):
+        """Calculate the band weights for a given set of redshifts
+
+        We have precomputed the weights for each bandpass, so we simply interpolate
+        those weights at the desired redshifts. We are working in log-wavelength, so a
+        change in redshift just gives us a shift in indices.
+
+        Parameters
+        ----------
+        redshifts : List[float]
+            Redshifts to calculate the band weights at
+
+        Returns
+        -------
+        `~numpy.ndarray`
+            Band weights for each redshift/band combination
+        """
+        # Figure out the locations to sample at for each redshift.
+        locs = (
+                self.band_interpolate_locations
+                + torch.log10(1 + redshifts)[:, None] / self.band_interpolate_spacing
+        )
+
+        flat_locs = locs.flatten()
+
+        # Linear interpolation
+        int_locs = flat_locs.long()
+        remainders = flat_locs - int_locs
+
+        start = self.band_interpolate_weights[..., int_locs]
+        end = self.band_interpolate_weights[..., int_locs + 1]
+
+        flat_result = remainders * end + (1 - remainders) * start
+        result = flat_result.reshape((-1,) + locs.shape).permute(1, 2, 0)
+
+        # We need an extra term of 1 + z from the filter contraction.
+        result /= (1 + redshifts)[:, None, None]
+
+        return result
+
+    def get_flux(self, theta, band_indices):
+        t = np.arange(-8, 40, 4)
+        t = np.r_[t, t, t, t]
+        ix = np.argsort(t)
+        t = t[ix]
+        J_t = torch.from_numpy(spline_utils.spline_coeffs_irr(t, self.tau_knots, self.KD_t).T)
+        J_t = J_t.to(self.device)
+        J_t = J_t.float()
+        J_t_hsiao = torch.from_numpy(spline_utils.spline_coeffs_irr(t, self.hsiao_t, self.KD_t_hsiao).T)
+        J_t_hsiao = J_t_hsiao.to(self.device)
+        J_t_hsiao = J_t_hsiao.float()
+
+        W = self.W0 + theta * self.W1
+        W = W.float()
+        WJt = torch.matmul(W, J_t)
+        W_grid = torch.matmul(self.J_l_T, WJt)
+
+        HJt = torch.matmul(self.hsiao_flux, J_t_hsiao)
+        H_grid = torch.matmul(self.J_l_T_hsiao, HJt)
+
+        model_spectra = H_grid * 10 ** W_grid
+        model_spectra = torch.reshape(model_spectra, (-1, *model_spectra.shape))
+
+        redshifts = torch.from_numpy(np.array([0]))
+
+        num_batches = 1
+        num_observations = t.shape[0]
+        band_weights = self._calculate_band_weights(redshifts)
+        batch_indices = (
+            torch.arange(num_batches, device=self.device)
+            .repeat_interleave(num_observations)
+        )
+
+        obs_band_weights = (
+            band_weights[batch_indices, :, band_indices.flatten()[ix]]
+            .reshape((num_batches, num_observations, -1))
+            .permute(0, 2, 1)
+        )
+
+        # print(obs_band_weights.shape)
+        # return
+
+        band_indices = band_indices.flatten()[ix]
+
+        model_flux = torch.sum(model_spectra * obs_band_weights, axis=1)
+        for i in range(4):
+            inds = band_indices == i
+            plt.scatter(t[inds], model_flux.detach().numpy().squeeze()[inds])
+        plt.show()
+
+    def model(self):
+        sample_size = 10
+        with pyro.plate("SNe", sample_size) as sn_index:
+            theta = pyro.sample("theta", dist.Normal(0, 1))
+
+    def fit(self, dataset):
+        self.process_dataset(dataset)
+        return
+
+    def process_dataset(self, dataset):
+        all_data = []
+        for lc in dataset.light_curves:
+            lc = lc.to_pandas()
+            lc = lc.astype({'band': str})
+            lc['band'] = lc['band'].apply(lambda band: band[band.find("'") + 1: band.rfind("'")])
+            lc['band'] = lc['band'].apply(lambda band: self.band_dict[band])
+            lc = lc.values
+            lc = np.reshape(lc, (-1, *lc.shape))
+            all_data.append(lc)
+        all_data = np.concatenate(all_data)
+        print(all_data.shape)
+
+
+
+
---
+++ train.py
@@ -0,0 +1,13 @@
+import numpy as np
+import lcdata
+import parsnip
+from model import Model
+
+dataset_path = 'data/bayesn_sim_test_z0_noext_25000.h5'
+dataset = lcdata.read_hdf5(dataset_path)[:100]
+bands = parsnip.get_bands(dataset)
+
+model = Model(bands)
+model.fit(dataset)
+t = 48
+band_indices = np.array([[i] * int(t/4) for i in range(4)])
