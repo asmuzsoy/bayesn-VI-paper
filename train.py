5bae43e0d6a78a1b88e29b8c03f6defdc67b29e2
mattgrayling <mjpg1g18@soton.ac.uk>
2022-11-23 16:32:30 +0000

    Updated for testing

--- train.py
+++ train.py
@@ -6,18 +6,23 @@ import parsnip
 from model import Model
 import pickle
 
-dataset_path = 'data/bayesn_sim_test_z0_noext_B_100.h5'
-dataset = lcdata.read_hdf5(dataset_path)[:10]
+dataset_path = 'data/bayesn_sim_test_z0_noext_thetarange_20.h5'
+dataset = lcdata.read_hdf5(dataset_path)
 bands = parsnip.get_bands(dataset)
 
-param_path = 'data/bayesn_sim_test_z0_noext_B_100_params.pkl'
+param_path = 'data/bayesn_sim_test_z0_noext_thetarange_20_params.pkl'
 params = pickle.load(open(param_path, 'rb'))
 del params['epsilon']
 params = pd.DataFrame(params)
 
 pd_dataset = dataset.meta.to_pandas()
 pd_dataset = pd_dataset.astype({'object_id': int})
+print(pd_dataset)
+inds = pd_dataset.object_id.values - 1
 params = params.merge(pd_dataset, on='object_id')
+print(params.theta.values[inds])
+print(params)
+raise ValueError('Nope')
 print('Actual', params.theta.values)
 
 model = Model(bands, device='cpu')
