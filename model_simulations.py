c2f38c69884bfd183e2a69b6203cb4bcf62ef272
mattgrayling <mjpg1g18@soton.ac.uk>
2022-12-08 14:36:04 +0000

    Updated simulation training code to give new outputs

--- model_simulations.py
+++ model_simulations.py
@@ -22,23 +22,25 @@ import yaml
 from astropy.cosmology import FlatLambdaCDM
 import matplotlib as mpl
 from matplotlib import rc
+import arviz
 
 rc('font', **{'family': 'serif', 'serif': ['cmr10']})
 mpl.rcParams['axes.unicode_minus'] = False
 mpl.rcParams['mathtext.fontset'] = 'cm'
 plt.rcParams.update({'font.size': 22})
 
-# jax.config.update('jax_platform_name', 'cpu')
+jax.config.update('jax_platform_name', 'cpu')
 # numpyro.set_host_device_count(4)
 
 print(jax.devices())
 
 
 class Model(object):
-    def __init__(self, bands, ignore_unknown_settings=False, settings={}, device='cuda',
+    def __init__(self, ignore_unknown_settings=False, settings={},
                  fiducial_cosmology={"H0": 73.24, "Om0": 0.28}):
         self.data = None
-        self.device = device
+        bands = ['ps1::g', 'ps1::r', 'ps1::i', 'ps1::z']
+        self.band_dict = {band[-1]: i for i, band in enumerate(bands)}
         self.settings = parse_settings(bands, settings,
                                        ignore_unknown_settings=ignore_unknown_settings)
         self.M0 = device_put(jnp.array(-19.5))
@@ -59,6 +61,7 @@ class Model(object):
         self.W1 = device_put(self.W1)
         self.L_Sigma = device_put(self.L_Sigma)
         self.band_dict = {band: i for i, band in enumerate(bands)}
+        self.RV_MW = 3.1
 
         self._setup_band_weights()
 
@@ -168,7 +171,7 @@ class Model(object):
         self.band_interpolate_weights = jnp.array(band_weights)
         self.model_wave = 10 ** (model_log_wave)
 
-    def _calculate_band_weights(self, redshifts):
+    def _calculate_band_weights(self, redshifts, ebv):
         """Calculate the band weights for a given set of redshifts
 
         We have precomputed the weights for each bandpass, so we simply interpolate
@@ -206,18 +209,23 @@ class Model(object):
         # We need an extra term of 1 + z from the filter contraction.
         result /= (1 + redshifts)[:, None, None]
 
+        # Apply MW extinction
+        abv = self.RV_MW * ebv
+        mw_array = jnp.zeros((result.shape[0], result.shape[1]))
+        for i, val in enumerate(abv):
+            mw = jnp.power(10, -0.4 * extinction.fitzpatrick99(self.model_wave, val, self.RV_MW))
+            mw_array = mw_array.at[i, :].set(mw)
+
+        result = result * mw_array[..., None]
+
         # Hack fix maybe
         sum = jnp.sum(result, axis=1)
         result /= sum[:, None, :]
 
         return result
 
-    def get_flux_batch(self, theta, Av, W0, W1, eps, Ds, redshifts, band_indices):
+    def get_flux_batch(self, theta, Av, W0, W1, eps, Ds, Rv, redshifts, ebv, band_indices, flag):
         num_batch = theta.shape[0]
-        J_t = jnp.reshape(self.J_t, (-1, *self.J_t.shape))
-        J_t = jnp.repeat(J_t, num_batch, axis=0)
-        J_t_hsiao = jnp.reshape(self.J_t_hsiao, (-1, *self.J_t_hsiao.shape))
-        J_t_hsiao = jnp.repeat(J_t_hsiao, num_batch, axis=0)
         W0 = jnp.reshape(W0, (-1, *self.W0.shape))
         W0 = jnp.repeat(W0, num_batch, axis=0)
         W1 = jnp.reshape(W1, (-1, *self.W1.shape))
@@ -225,30 +233,29 @@ class Model(object):
 
         W = W0 + theta[..., None, None] * W1 + eps
 
-        WJt = jnp.matmul(W, J_t)
+        WJt = jnp.matmul(W, self.J_t)
         W_grid = jnp.matmul(self.J_l_T, WJt)
 
-        HJt = jnp.matmul(self.hsiao_flux, J_t_hsiao)
+        HJt = jnp.matmul(self.hsiao_flux, self.J_t_hsiao)
         H_grid = jnp.matmul(self.J_l_T_hsiao, HJt)
 
         model_spectra = H_grid * 10 ** (-0.4 * W_grid)
 
         num_observations = band_indices.shape[0]
 
-        band_weights = self._calculate_band_weights(redshifts)
+        # band_weights = self._calculate_band_weights(redshifts, ebv)
         batch_indices = (
             jnp.arange(num_batch)
             .repeat(num_observations)
         ).astype(int)
 
         obs_band_weights = (
-            band_weights[batch_indices, :, band_indices.T.flatten()]
+            self.band_weights[batch_indices, :, band_indices.T.flatten()]
             .reshape((num_batch, num_observations, -1))
             .transpose(0, 2, 1)
         )
 
         # Extinction----------------------------------------------------------
-        Rv = 2.610
         f99_x0 = 4.596
         f99_gamma = 0.99
         f99_c2 = -0.824 + 4.717 / Rv
@@ -275,10 +282,9 @@ class Model(object):
         model_spectra = model_spectra * f_A[..., None]
 
         model_flux = jnp.sum(model_spectra * obs_band_weights, axis=1).T
-
         model_flux = model_flux * 10 ** (-0.4 * (self.M0 + Ds))
-
         model_flux *= self.device_scale
+        model_flux *= flag
 
         return model_flux
 
@@ -424,38 +430,166 @@ class Model(object):
         L_Omega = numpyro.sample('L_Omega', dist.LKJCholesky(N_knots_sig))
         L_Sigma = jnp.matmul(jnp.diag(sigmaepsilon), L_Omega)
         sigma0 = numpyro.sample('sigma0', dist.HalfCauchy(0.1))
+        Rv = numpyro.sample('Rv', dist.Uniform(1, 5))
+        tauA = numpyro.sample('tauA', dist.HalfCauchy())
 
         # for sn_index in pyro.plate('SNe', sample_size):
         with numpyro.plate('SNe', sample_size) as sn_index:
             theta = numpyro.sample(f'theta', dist.Normal(0, 1.0))  # _{sn_index}
-            # Rv = numpyro.sample(f'RV', dist.Normal(2.610, 0.001))
-            Av = numpyro.sample(f'AV', dist.Exponential(1 / 0.194))
+            Av = numpyro.sample(f'AV', dist.Exponential(1 / tauA))
             eps_mu = jnp.zeros(N_knots_sig)
             eps = numpyro.sample('eps', dist.MultivariateNormal(eps_mu, scale_tril=L_Sigma))
             eps = jnp.reshape(eps, (sample_size, self.l_knots.shape[0] - 2, self.tau_knots.shape[0]), order='F')
             eps_full = jnp.zeros((sample_size, self.l_knots.shape[0], self.tau_knots.shape[0]))
             eps = eps_full.at[:, 1:-1, :].set(eps)
-            band_indices = obs[-3, :, sn_index].astype(int).T
-            redshift = obs[-2, 0, sn_index]
-            muhat = obs[-1, 0, sn_index]
-            muhat_err = 5 / (redshift * jnp.log(10)) * self.sigma_pec
+            band_indices = obs[-6, :, sn_index].astype(int).T
+            redshift = obs[-5, 0, sn_index]
+            redshift_error = obs[-4, 0, sn_index]
+            muhat = obs[-3, 0, sn_index]
+            ebv = obs[-2, 0, sn_index]
+            flag = obs[-1, :, sn_index].T
+            muhat_err = 5 / (redshift * jnp.log(10)) * jnp.sqrt(
+                jnp.power(redshift_error, 2) + np.power(self.sigma_pec, 2))
             Ds_err = jnp.sqrt(muhat_err * muhat_err + sigma0 * sigma0)
             Ds = numpyro.sample('Ds', dist.Normal(muhat, Ds_err))
-            start = time.time()
-            flux = self.get_flux_batch(theta, Av, W0, W1, eps, Ds, redshift, band_indices)
-
-            end = time.time()
-            elapsed = end - start
+            flux = self.get_flux_batch(theta, Av, W0, W1, eps, Ds, Rv, redshift, ebv, band_indices, flag)
             numpyro.sample(f'obs', dist.Normal(flux, obs[2, :, sn_index].T), obs=obs[1, :, sn_index].T)  # _{sn_index}
 
-    def train(self, dataset):
-        self.process_dataset(dataset)
-        rng = PRNGKey(123)
+    def initial_guess(self, n_chains=1, reference_model="M20", RV_init=3.0, tauA_init=0.3):
+        # Set hyperparameter initialisations
+        # Set initialisations (these will be jittered chain by chain)
+        param_root = 'model_files/T21_model'
+        W0_init = np.loadtxt(f'{param_root}/W0.txt')
+        n_lknots, n_tauknots = W0_init.shape
+        W0_init = W0_init.flatten(order='F')
+        W1_init = np.loadtxt(f'{param_root}/W1.txt').flatten(order='F')
+        RV_init, tauA_init = np.loadtxt(f'{param_root}/M0_sigma0_RV_tauA.txt')[[2, 3]]
+
+        # I should remove all of this hardcoding
+        n_eps = (n_lknots - 2) * n_tauknots
+        sigma0_init = 0.1
+        sigmaepsilon_init = 0.1 * np.ones(n_eps)
+        L_Omega_init = np.eye(n_eps)
+
+        n_sne = self.data.shape[-1]
+
+        # Prepare initial guesses
+        # I should make the jittering more free for the user to control
+        param_init = {
+            'W0': np.zeros((n_chains, *W0_init.shape)),
+            'W1': np.zeros((n_chains, *W1_init.shape)),
+            'Rv': np.zeros(n_chains),
+            'tauA': np.zeros(n_chains),
+            'sigma0': np.zeros(n_chains),
+            'sigmaepsilon': np.zeros((n_chains, *sigmaepsilon_init.shape)),
+            'L_Omega': np.zeros((n_chains, *L_Omega_init.shape)),
+            'Av': np.zeros((n_chains, n_sne)),
+            'theta': np.zeros((n_chains, n_sne)),
+            'epsilon': np.zeros((n_chains, n_sne, n_eps)),
+            'Ds': np.zeros((n_chains, n_sne))
+        }
+        param_init = {}
+        tauA_ = tauA_init + np.random.normal(0, 0.01)
+        while tauA_ < 0:
+            tauA_ = tauA_init + np.random.normal(0, 0.01)
+        sigma0_ = sigma0_init + np.random.normal(0, 0.01)
+        param_init['W0'] = W0_init + np.random.normal(0, 0.01, W0_init.shape[0])
+        param_init['W1'] = W1_init + np.random.normal(0, 0.01, W1_init.shape[0])
+        param_init['Rv'] = RV_init  # + np.random.uniform(1.0 - RV_init, 5.0 - RV_init)
+        param_init['tauA'] = tauA_
+        param_init['sigma0'] = sigma0_
+        param_init['sigmaepsilon'] = sigmaepsilon_init + np.random.normal(0, 0.01, sigmaepsilon_init.shape)
+        param_init['L_Omega'] = L_Omega_init
+        param_init['theta'] = np.random.normal(0, 1, n_sne)
+        param_init['Av'] = np.random.exponential(tauA_, n_sne)
+        param_init['epsilon'] = np.random.normal(0, 1, (n_sne, n_eps))
+        param_init['Ds'] = np.random.normal(self.data[-3, 0, :], sigma0_)
+
+        return param_init
+
+    def train(self, num_samples, num_warmup, num_chains, output, chain_method='parallel', init_strategy='median'):
+        self.process_dataset(mode='training')
+        if init_strategy == 'value':
+            init_strategy = init_to_value(values=self.initial_guess())
+        elif init_strategy == 'median':
+            init_strategy = init_to_median()
+        elif init_strategy == 'sample':
+            init_strategy = init_to_sample()
+        else:
+            raise ValueError('Invalid init strategy, must be one of value, median and sample')
+        self.band_weights = self._calculate_band_weights(self.data[-4, 0, :], self.data[-2, 0, :])
+        # rng = jnp.array([PRNGKey(1), PRNGKey(2), PRNGKey(3), PRNGKey(4)])
+        rng = PRNGKey(101)
         # numpyro.render_model(self.train_model, model_args=(self.data,), filename='train_model.pdf')
-        nuts_kernel = NUTS(self.train_model, adapt_step_size=True, target_accept_prob=0.9, init_strategy=init_to_median())
-        mcmc = MCMC(nuts_kernel, num_samples=250, num_warmup=250, num_chains=1)
+        nuts_kernel = NUTS(self.train_model, adapt_step_size=True, target_accept_prob=0.8, init_strategy=init_strategy)
+        mcmc = MCMC(nuts_kernel, num_samples=num_samples, num_warmup=num_warmup, num_chains=num_chains,
+                    chain_method=chain_method)
         mcmc.run(rng, self.data)
-        return mcmc
+        mcmc.print_summary()
+        samples = mcmc.get_samples(group_by_chain=True)
+        self.train_postprocess(samples, output)
+
+    def train_postprocess(self, samples, output):
+        if not os.path.exists(os.path.join('results', output)):
+            os.mkdir(os.path.join('results', output))
+        with open(os.path.join('results', output, 'initial_chains.pkl'), 'wb') as file:
+            pickle.dump(samples, file)
+        # Sign flipping-----------------
+        J_R = spline_utils.spline_coeffs_irr([6200.0], self.l_knots, spline_utils.invKD_irr(self.l_knots))
+        J_10 = spline_utils.spline_coeffs_irr([10.0], self.tau_knots, spline_utils.invKD_irr(self.tau_knots))
+        J_0 = spline_utils.spline_coeffs_irr([0.0], self.tau_knots, spline_utils.invKD_irr(self.tau_knots))
+        W1 = np.reshape(samples['W1'], (
+        samples['W1'].shape[0], samples['W1'].shape[1], self.l_knots.shape[0], self.tau_knots.shape[0]), order='F')
+        N_chains = W1.shape[0]
+        sign = np.zeros(N_chains)
+        for chain in range(N_chains):
+            chain_W1 = np.mean(W1[chain, ...], axis=0)
+            chain_sign = np.sign(
+                np.squeeze(np.matmul(J_R, np.matmul(chain_W1, J_10.T))) - np.squeeze(
+                    np.matmul(J_R, np.matmul(chain_W1, J_0.T))))
+            sign[chain] = chain_sign
+        samples["W1"] = samples["W1"] * sign[:, None, None]
+        samples["theta"] = samples["theta"] * sign[:, None, None]
+        # Modify W1 and theta----------------
+        theta_std = np.std(samples["theta"], axis=2)
+        samples['theta'] = samples['theta'] / theta_std[..., None]
+        samples['W1'] = samples['W1'] * theta_std[..., None]
+        # Save convergence data for each parameter to csv file
+        summary = arviz.summary(samples)
+        summary.to_csv(os.path.join('results', output, 'fit_summary.csv'))
+        with open(os.path.join('results', output, 'chains.pkl'), 'wb') as file:
+            pickle.dump(samples, file)
+        # Save best fit global params to files for easy inspection and reading in------
+        W0 = np.mean(samples['W0'], axis=[0, 1]).reshape((self.l_knots.shape[0], self.tau_knots.shape[0]), order='F')
+        W1 = np.mean(samples['W1'], axis=[0, 1]).reshape((self.l_knots.shape[0], self.tau_knots.shape[0]),
+                                                         order='F')
+        sigmaepsilon = np.mean(samples['sigmaepsilon'], axis=[0, 1])
+        L_Omega = np.mean(samples['L_Omega'], axis=[0, 1])
+        L_Sigma = np.matmul(np.diag(np.mean(samples['sigmaepsilon'], axis=[0, 1])),
+                            np.mean(samples['L_Omega'], axis=[0, 1]))
+        sigma0 = np.mean(samples['sigma0'])
+        Rv = np.mean(samples['Rv'])
+        tauA = np.mean(samples['tauA'])
+        M0_sigma0_RV_tauA = np.array([self.M0, sigma0, Rv, tauA])
+        np.savetxt(os.path.join('results', output, 'W0.txt'), W0, delimiter="\t", fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'W1.txt'), W1, delimiter="\t", fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'sigmaepsilon.txt'), sigmaepsilon, delimiter="\t", fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'L_Omega.txt'), L_Omega, delimiter="\t", fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'L_Sigma.txt'), L_Sigma, delimiter="\t", fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'M0_sigma0_RV_tauA.txt'), M0_sigma0_RV_tauA, delimiter="\t",
+                   fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'l_knots.txt'), self.l_knots, delimiter="\t", fmt="%.3f")
+        np.savetxt(os.path.join('results', output, 'tau_knots.txt'), self.tau_knots, delimiter="\t", fmt="%.3f")
+
+        """global_param_dict = {
+            'W0': repr(np.round(np.mean(samples['W0'], axis=[0, 1]).reshape((self.l_knots.shape[0], self.tau_knots.shape[0]), order='F'), 3).tolist()),
+            'W1': repr(np.mean(samples['W1'], axis=[0, 1]).reshape((self.l_knots.shape[0], self.tau_knots.shape[0]),
+                                                              order='F').tolist()),
+            'sigmaepsilon': repr(np.mean(samples['sigmaepsilon'], axis=[0, 1])),
+            'L_Omega': repr(np.mean(samples['L_Omega'], axis=[0, 1])),
+            'L_Sigma': repr(np.matmul(np.diag(np.mean(samples['sigmaepsilon'], axis=[0, 1])), np.mean(samples['L_Omega'], axis=[0, 1]))),
+            'sigma0': np.mean(samples['sigma0'])
+        }"""
 
     def train_assess(self, params, yaml_dir):
         with open(os.path.join('results', f'{yaml_dir}.yaml'), 'r') as file:
@@ -544,20 +678,33 @@ class Model(object):
         plt.subplots_adjust(hspace=0, wspace=0)
         plt.show()
 
-    def process_dataset(self, dataset):
+    def process_dataset(self, mode='training'):
+        dataset_path = 'data/bayesn_sim_team_z0.1_daily_25000.h5'
+        dataset = lcdata.read_hdf5(dataset_path)[:157]
+        param_path = 'data/bayesn_sim_team_z0.1_daily_25000_params.csv'
+        params = pd.read_csv(param_path)
+        pd_dataset = dataset.meta.to_pandas()
+        pd_dataset = pd_dataset.astype({'object_id': int})
+        params = pd_dataset.merge(params, on='object_id')
         all_data = []
         self.t = None
         for lc_ind, lc in enumerate(dataset.light_curves):
             lc = lc.to_pandas()
             lc = lc.astype({'band': str})
-            lc[['flux', 'fluxerr']] = lc[['flux', 'fluxerr']]  * self.scale
+            lc['t'] = lc['time']
+            lc[['flux', 'flux_err']] = lc[['flux', 'fluxerr']]  * self.scale
             lc['band'] = lc['band'].apply(lambda band: band[band.find("'") + 1: band.rfind("'")])
             lc['band'] = lc['band'].apply(lambda band: self.band_dict[band])
             lc['redshift'] = dataset.meta[lc_ind][-1]
             lc['dist_mod'] = self.cosmo.distmod(dataset.meta[lc_ind][-1])
-            lc = lc.sort_values('time')
+            lc['band_indices'] = lc.band
+            lc['redshift_error'] = 1e-5
+            lc['MWEBV'] = 0
+            lc['flag'] = 1
+            lc = lc[['t', 'flux', 'flux_err', 'band_indices', 'redshift', 'redshift_error', 'dist_mod', 'MWEBV', 'flag']]
+            lc = lc.sort_values('t')
             if self.t is None:
-                self.t = lc.time.values
+                self.t = lc.t.values
             lc = lc.values
             lc = np.reshape(lc, (-1, *lc.shape))
             all_data.append(lc)
@@ -665,25 +812,12 @@ def get_band_effective_wavelength(band):
 
 
 if __name__ == '__main__':
-    dataset_path = 'data/bayesn_sim_team_z0.1_daily_25000.h5'
-    dataset = lcdata.read_hdf5(dataset_path)[:1000]
-    bands = set()
-    for lc in dataset.light_curves:
-        bands = bands.union(lc['band'])
-    bands = np.array(sorted(bands, key=get_band_effective_wavelength))
-
-    param_path = 'data/bayesn_sim_team_z0.1_daily_25000_params.csv'
-    params = pd.read_csv(param_path)
-
-    pd_dataset = dataset.meta.to_pandas()
-    pd_dataset = pd_dataset.astype({'object_id': int})
-    params = pd_dataset.merge(params, on='object_id')
-
-    model = Model(bands, device='cuda')
-    # result = model.fit(dataset)
-    # result = model.train(dataset)
+    model = Model()
+    # model.fit(250, 250, 4, 'foundation_fit_4chain', 'foundation_train_Rv')
+    model.train(250, 250, 4, 'simulation_train', chain_method='vectorized', init_strategy='median')
+    # model.train_postprocess()
     # result.print_summary()
-    # model.save_results_to_yaml(result, 'daily_cadence_train')
+    # model.save_results_to_yaml(result, 'foundation_train_4chain')
     # model.fit_assess(params, '4chain_fit_test')
-    # model.fit_from_results(dataset, 'gpu_train_dist')
-    model.train_assess(params, '4chain_train_test')
+    # model.fit_from_results('foundation_train')
+    # model.train_assess(params, 'gpu_train_dist')
