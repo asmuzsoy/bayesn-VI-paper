5cdbe9ceefe6e7336f6322c7f7ef4a7527bb2ef4
mattgrayling <mjpg1g18@soton.ac.uk>
2023-01-27 10:50:25 +0000

    Rerunning foundation training

--- model_foundation.py
+++ model_foundation.py
@@ -973,7 +973,7 @@ class Model(object):
 if __name__ == '__main__':
     model = Model()
     # model.fit(250, 250, 4, 'foundation_fit_4chain', 'foundation_train_500_initval', chain_method='vectorized')
-    model.train(500, 500, 4, 'foundation_train_500_val', chain_method='sequential', init_strategy='value')
+    model.train(1000, 1000, 4, 'foundation_train_1000_val', chain_method='sequential', init_strategy='value')
     # model.simulate_spectrum()
     # model.train_postprocess()
     # result.print_summary()
