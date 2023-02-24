1c6b6b4d442c200835ddeb8e148c9a92d766593d
mattgrayling <mjpg1g18@soton.ac.uk>
2023-02-24 17:10:33 +0000

    Checking zero pints

--- zp_check.py
+++ zp_check.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
+from scipy.integrate import simpson
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
 with fits.open('data/zp/alpha_lyr_stis_008.fits') as hdu:
     head = hdu[0].header
     df = pd.DataFrame.from_records(hdu[1].data)
-print(df)
-plt.plot(df.WAVELENGTH, df.FLUX)
-plt.xlim(9e2, 1e4)
-plt.show()
+
+R = np.loadtxt(f'data/filters/LCO/Swope/B_tel_ccd_atm_ext_1.2.dat')
+R[:, 1] = R[:, 1] * (R[:, 1] > 0.002 * R[:, 1].max())
+lam = R[:, 0]
+vega = interp1d(df.WAVELENGTH, df.FLUX, kind='cubic')(lam)
+int1 = simpson(lam * vega * R[:, 1], lam)
+int2 = simpson(lam * R[:, 1], lam)
+zp_flux = int1 / int2
+zp_mag = -2.5 * np.log10(zp_flux)
+print(zp_flux, zp_mag)
