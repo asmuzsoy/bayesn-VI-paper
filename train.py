0273853cf5963a6cfc01f90289552f9d347d5713
mattgrayling <mjpg1g18@soton.ac.uk>
2023-03-13 18:56:03 +0000

    Fixed typo

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ model = SEDmodel(load_model='M20_model')
 #filt_map_dict = {'g': 'g_PS1', 'r': 'r_PS1', 'i': 'i_PS1', 'z': 'z_PS1'}
 #model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/LCs/meta/T21_training_set_meta.txt',
 #                      filt_map_dict, data_mode='mag')
-model.process_dataset('M20', 'data/lcs/tables/M20_training_set.txt', 'data/LCs/meta/M20_training_set_meta.txt',
+model.process_dataset('M20', 'data/lcs/tables/M20_training_set.txt', 'data/lcs/meta/M20_training_set_meta.txt',
                       data_mode='mag')
 #model.process_dataset('ztf', 'data/LCs/ZTF', 'data/LCs/meta/ztf_dr1_training.txt',
 #                      map_dict=None, data_mode='mag')
