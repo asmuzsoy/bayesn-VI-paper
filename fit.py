72e5c6c5d7253c109dab6ab7420048de9391f5aa
mattgrayling <mjpg1g18@soton.ac.uk>
2023-03-17 13:40:41 +0000

    Profiling Perlmutter

--- fit.py
+++ fit.py
@@ -6,7 +6,7 @@ model = SEDmodel()
 #model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/lcs/meta/T21_training_set_meta.txt',
 #                      filt_map_dict, data_mode='mag')
 
-model.process_dataset('T21_sim_1000', 'data/lcs/tables/T21_sim_1000.txt', 'data/LCs/meta/T21_sim_1000_meta.txt',
+model.process_dataset('T21_sim_1000', 'data/lcs/tables/T21_sim_1000.txt', 'data/lcs/meta/T21_sim_1000_meta.txt',
                       data_mode='flux')
 
 model.fit(250, 250, 4, 'T21_sim_1000_fit', chain_method='parallel')
