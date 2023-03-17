65dbc153b7c7724c2a9b8be73a8e6aa70193a5fc
mattgrayling <mjpg1g18@soton.ac.uk>
2023-03-17 11:42:23 +0000

    Profiling Perlmutter

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from bayesn_model import SEDmodel
 model = SEDmodel(load_model='T21_model')
 
 #filt_map_dict = {'g': 'g_PS1', 'r': 'r_PS1', 'i': 'i_PS1', 'z': 'z_PS1'}
-#model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/LCs/meta/T21_training_set_meta.txt',
+#model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/lcs/meta/T21_training_set_meta.txt',
 #                      filt_map_dict, data_mode='mag')
 
 model.process_dataset('T21_sim_10000', 'data/lcs/tables/T21_sim_10000.txt', 'data/LCs/meta/T21_sim_10000_meta.txt',
@@ -12,7 +12,7 @@ model.process_dataset('T21_sim_10000', 'data/lcs/tables/T21_sim_10000.txt', 'dat
 #model.process_dataset('M20', 'data/lcs/tables/M20_training_set.txt', 'data/lcs/meta/M20_training_set_meta.txt',
 #                      data_mode='mag')
 
-#model.process_dataset('ztf', 'data/LCs/ZTF', 'data/LCs/meta/ztf_dr1_training.txt',#            l_knots=[4150, 4760, 6390, 7930, 9000])
+#model.process_dataset('ztf', 'data/lcs/ZTF', 'data/LCs/meta/ztf_dr1_training.txt',#            l_knots=[4150, 4760, 6390, 7930, 9000])
 #                      map_dict=None, data_mode='mag')
 
 model.train(500, 500, 4, 'T21_sim_10000_train', chain_method='parallel', init_strategy='T21')
