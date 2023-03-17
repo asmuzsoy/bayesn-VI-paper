df8035fc18a10281990282d6c826f17da624c81e
mattgrayling <mjpg1g18@soton.ac.uk>
2023-03-17 00:13:27 +0000

    Profiling Perlmutter

--- fit.py
+++ fit.py
@@ -3,5 +3,5 @@ from bayesn_model import SEDmodel
 model = SEDmodel()
 filt_map_dict = {'g': 'g_PS1', 'r': 'r_PS1', 'i': 'i_PS1', 'z': 'z_PS1'}
 model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/LCs/meta/T21_training_set_meta.txt',
-                      filt_map_dict, data_mode='mag')
+                      filt_map_dict, data_mode='flux')
 model.fit(250, 250, 4, 'T21_fit', chain_method='parallel')
