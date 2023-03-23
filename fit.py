99b633ccef2a97c67243f5a8c816113f2266dedf
mattgrayling <mjpg1g18@soton.ac.uk>
2023-03-23 11:53:14 +0000

    Fitting on YSE

--- fit.py
+++ fit.py
@@ -6,7 +6,9 @@ model = SEDmodel()
 #model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/lcs/meta/T21_training_set_meta.txt',
 #                      filt_map_dict, data_mode='mag')
 
-model.process_dataset('T21_sim_10000', 'data/lcs/tables/T21_sim_10000.txt', 'data/lcs/meta/T21_sim_10000_meta.txt',
-                      data_mode='flux')
+#model.process_dataset('T21_sim_10000', 'data/lcs/tables/T21_sim_10000.txt', 'data/lcs/meta/T21_sim_10000_meta.txt',
+#                      data_mode='flux')
 
-model.fit(250, 250, 4, 'T21_sim_1000_fit', chain_method='parallel', init_strategy='median')
+model.process_dataset('YSE_DR1', 'data/lcs/tables/YSE_DR1_table.txt', 'data/lcs/meta/YSE_DR1_meta.txt', data_mode='flux')
+
+model.fit(250, 250, 4, 'YSE_fit', chain_method='parallel', init_strategy='median')
