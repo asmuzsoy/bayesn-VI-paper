02b2cf4bbeae0082684cf01ace5a899c1470a94e
mattgrayling <mjpg1g18@soton.ac.uk>
2023-05-03 15:59:10 +0100

    Benchmarking T21 fits

--- simulate_sed.py
+++ simulate_sed.py
@@ -3,10 +3,10 @@ from timeit import default_timer
 from bayesn_model import SEDmodel
 
 model = SEDmodel(load_model='T21_model')
-N = 10000
+N = 100
 start = default_timer()
 lc, params = model.simulate_light_curve(np.arange(-8, 40, 4), N, ['g_PS1', 'r_PS1', 'i_PS1', 'z_PS1'],
-                                        z=np.random.uniform(0, 0.1, N), mu='z', write_to_files=False)#, sim_name='T21_sim_10000')
+                                        z=np.random.uniform(0, 0.1, N), mu='z', write_to_files=True, sim_name='T21_sim_100')
 end = default_timer()
 print(f'Simulating {N} objects took {end - start} seconds')
 print(lc.shape)
