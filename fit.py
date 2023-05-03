5aee95521488e43d22f7bc60511e2847697f0251
mattgrayling <mjpg1g18@soton.ac.uk>
2023-05-03 16:02:25 +0100

    Benchmarking T21 fits

--- fit.py
+++ fit.py
@@ -2,12 +2,12 @@ from bayesn_model import SEDmodel
 
 model = SEDmodel(load_model='T21_model')
 
-filt_map_dict = {'g': 'g_PS1', 'r': 'r_PS1', 'i': 'i_PS1', 'z': 'z_PS1'}
-model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/lcs/meta/T21_training_set_meta.txt',
-                      filt_map_dict, data_mode='flux')
+#filt_map_dict = {'g': 'g_PS1', 'r': 'r_PS1', 'i': 'i_PS1', 'z': 'z_PS1'}
+#model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/lcs/meta/T21_training_set_meta.txt',
+#                      filt_map_dict, data_mode='flux')
 
-#model.process_dataset('T21_sim_10000', 'data/lcs/tables/T21_sim_10000.txt', 'data/lcs/meta/T21_sim_10000_meta.txt',
-#                      data_mode='flux')
+model.process_dataset('T21_sim_100', 'data/lcs/tables/T21_sim_100.txt', 'data/lcs/meta/T21_sim_100_meta.txt',
+                      data_mode='flux')
 
 #model.process_dataset('YSE_DR1', 'data/lcs/tables/YSE_DR1_table.txt', 'data/lcs/meta/YSE_DR1_meta.txt', data_mode='flux')
 
