3693ecf1199d06f6d08e2721c360d2d0c694f331
mattgrayling <mjpg1g18@soton.ac.uk>
2023-05-03 16:12:40 +0100

    Benchmarking T21 fits

--- fit.py
+++ fit.py
@@ -6,7 +6,7 @@ model = SEDmodel(load_model='T21_model')
 #model.process_dataset('foundation', 'data/lcs/tables/T21_training_set.txt', 'data/lcs/meta/T21_training_set_meta.txt',
 #                      filt_map_dict, data_mode='flux')
 
-model.process_dataset('T21_sim_100', 'data/lcs/tables/T21_sim_100.txt', 'data/lcs/meta/T21_sim_100_meta.txt',
+model.process_dataset('T21_sim_1000', 'data/lcs/tables/T21_sim_1000.txt', 'data/lcs/meta/T21_sim_1000_meta.txt',
                       data_mode='flux')
 
 #model.process_dataset('YSE_DR1', 'data/lcs/tables/YSE_DR1_table.txt', 'data/lcs/meta/YSE_DR1_meta.txt', data_mode='flux')
@@ -18,4 +18,4 @@ model.process_dataset('T21_sim_100', 'data/lcs/tables/T21_sim_100.txt', 'data/lc
 #model.process_dataset('YSEwZTF_Foundation', 'data/lcs/tables/YSEwZTF_Foundation_table.txt', 'data/lcs/meta/YSEwZTF_Foundation_meta.txt', data_mode='flux',
 #                      map_dict=filt_map_dict)
 
-model.fit(250, 250, 4, 'T21_fit', chain_method='parallel', init_strategy='median')
+model.fit(250, 250, 4, 'T21_sim_1000_fit', chain_method='parallel', init_strategy='median')
